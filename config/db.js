af1ab3c0086700bfcefbee56bae075b1d0e58854
nirmal <nirmalmoses14@gmail.com>
2025-02-26 18:22:42 +0530

    Save

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ async function connectDB() {
             bufferCommands:false
         }
 
-        cached.promise = (await mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts)).then(mongoose =>{
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose =>{
             return mongoose
         })
 
